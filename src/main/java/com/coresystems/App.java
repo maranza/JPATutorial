6f8663c6b60b25cf205d3a4aed3ca1327dfef433
maranza <o.maranza@gmail.com>
2019-01-30 23:22:22 +0200

    implement listStudents method, however not tested yet, due to JPA Facet

--- src/main/java/com/coresystems/App.java
+++ src/main/java/com/coresystems/App.java
@@ -1,13 +1,62 @@
 package com.coresystems;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.coresystems.models.Module;
+import com.coresystems.models.Student;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	@SuppressWarnings("unused")
+	private static final EntityManagerFactory emFactory;
+	
+	static {
+		emFactory = Persistence.createEntityManagerFactory("jpatutorial");
+	}
+	
+	private static EntityManager manager = null;
+	
+	// TODO: add the student to database	
+	public static void addStudent(String firstName, String lastName) {
+		
+	}
+	
+	// TODO: assign a module to student, one student can have multiple modules
+	public static void registerModule(Integer studentId, String moduleNam) {
+		
+	}
+	
+	//TODO: get all students
+	public static void listStudents() {
+		try {
+			manager = emFactory.createEntityManager();
+			List<Student> students = manager.createNativeQuery("SELECT s FROM Student s", Student.class).getResultList();
+			for (Student student : students) {
+				System.out.println("fistName: " + student.getFirstName() + "lastName: " + student.getLastName());
+				System.out.println("modules: ");
+				for (Module module : student.getModules()) {
+					System.out.println("Module name: " + module.getName());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			manager.close();
+			emFactory.close();
+		}
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        listStudents();
     }
 }
